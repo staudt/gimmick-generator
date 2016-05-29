314d90ad3fc668837e5c67727a0593d953535fd6
Ricardo Staudt <ricardost@gmail.com>
2016-05-29 15:51:47 -0300

    Better besign

--- js/main.js
+++ js/main.js
@@ -3,25 +3,38 @@ function getRandom(list) {
     return list[Math.floor( Math.random()*list.length )]
 }
 
-reveals = ["",
-	"illegitimate",
-	"soon to be revealed"
-];
+function generate() {
+    reveals = ["",
+    	"illegitimate",
+    	"soon to be revealed"
+    ];
 
-excuses = [
-	"I'm Vince McMahon's illegitimate son, so I demand a",
-	"I know a Stephanie McMahon dirty secret and I demand a"
-];
 
-gimmick = [
-    "a " + getRandom(jobs),
-    "the " + getRandom(reveals) +" son of " + getRandom(famousWrestlers),
-    "the " + getRandom(jobs),
-    "the second comming of " + getRandom(famousWrestlers),
-    "the " + getRandom(categories),
-    "a " + getRandom(categories)
-];
+    callsforexcuse = ["Wait!",
+        "Hold on!",
+        "No, no, no!"
+    ];
 
-document.getElementById("name").innerHTML = getRandom(firstNames) + ' ' + getRandom(secondNames);
-document.getElementById("gimmick").innerHTML =getRandom(gimmick);
-document.getElementById("excuse").innerHTML =getRandom(excuses);
+    excuses = [
+    	"I'm Vince McMahon's illegitimate son, so I demand a",
+    	"I know a Stephanie McMahon dirty secret and I demand a",
+        "I have a winning lawsuit againt WWE and want a",
+        "I'm friends with JBL, therefore I get a"
+    ];
+
+    gimmick = [
+        "a " + getRandom(jobs),
+        "the " + getRandom(reveals) +" son of " + getRandom(famousWrestlers),
+        "the " + getRandom(jobs),
+        "the second coming of " + getRandom(famousWrestlers),
+        "the " + getRandom(categories),
+        "a " + getRandom(categories)
+    ];
+
+    document.getElementById("name").innerHTML = getRandom(firstNames) + ' ' + getRandom(secondNames);
+    document.getElementById("gimmick").innerHTML =getRandom(gimmick);
+    document.getElementById("callforexcuse").innerHTML =getRandom(callsforexcuse);
+    document.getElementById("excuse").innerHTML =getRandom(excuses);
+}
+
+generate();
