6190ab2abc8a89b6a4f17d2925e460187a32a207
Ricardo Staudt <ricardost@gmail.com>
2016-05-29 14:36:59 -0300

    More random values

--- js/categories.js
+++ js/categories.js
@@ -1,1 +1,1 @@
-categories = ['Luchador', 'British Catch As Catchs Can Wrestler', 'European Guy', 'Russian', 'Mid Eastern Heel', 'Good Looking Dude', 'Borderline Bi-Sexual', 'Psycho', 'Generic English Man', 'Generic Comedian'];
+categories = ['Luchador', 'British Catch As Catchs Can Wrestler', 'European Guy', 'Russian', 'Mid Eastern Heel', 'Good Looking Dude', 'Borderline Bi-Sexual', 'Psycho', 'Generic English Man', 'Generic Comedian', 'Street Brawler', 'Conservative right-wing', 'Samoan', 'Red Neck', 'Wild Savage', 'Native American', 'Evil Arabian', 'Thug', 'Street Gangster'];
